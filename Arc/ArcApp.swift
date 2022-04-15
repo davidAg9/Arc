45e541e88e6dbd7252fee5a1dfc8d8d1894cacfb
davidAg9 <shane_alpha9@outlook.com>
2022-04-15 02:50:05 +0000

    Initial Commit

---
+++ Arc/ArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcApp.swift
+//  Arc
+//
+//  Created by Dave Agyakwa on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
